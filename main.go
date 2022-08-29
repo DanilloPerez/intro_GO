744f5f145b3698b56b9926a91da8dad013f07604
Danillo Perez <danillo0101@live.nl>
2022-08-29 13:48:39 +0200

    Added getDaypart function, edited welcome message

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	currentTime := time.Now()
+	dayPart := getDaypart(currentTime)
+	fmt.Println("good", dayPart)
+	fmt.Println("The current time is:", currentTime)
+}
+
+func getDaypart(datetime time.Time) string {
+	if datetime.Hour() < 12 {
+		return "morning"
+	}
+	if datetime.Hour() < 18 {
+		return "afternoon"
+	}
+	return "evening"
 }
